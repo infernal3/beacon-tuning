2d9be16ca62a71f3e255604ad635b1f8ab935926
infernal3 <infernal1559@gmail.com>
2025-06-30 17:35:12 -0500

    Add a beacon upgrade

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 const el=e=>document.getElementById(e);
 const previousTickArray = [0,0];
 
-var beaconSpeed, beaconFreq, userSpeed, userFreq,completions=-1;
+var beaconSpeed, beaconFreq, userSpeed, userFreq,completions=-1,flux=0,u1=0;
       function randomize(){
             beaconSpeed=1+parseInt(Math.random()*5);
             beaconFreq=200+(20*parseInt(Math.random()*20));
             userSpeed=3;
             userFreq=440;
             el("F").style="display:none";
-            completions++;
+            completions+=u1+1;
             el("T").textContent=completions;
       }
       randomize();
@@ -25,28 +25,35 @@ var beaconSpeed, beaconFreq, userSpeed, userFreq,completions=-1;
       }
       
       window.setInterval(()=>{
+        flux+=completions*0.05;
+        el("beacon-flux").textContent=Math.floor(flux);
         el("C").textContent=userFreq;
         el("D").textContent=`${userSpeed} (${(1.5/userSpeed).toFixed(3)}s intervals)`;
         let xw=previousTickArray[1]-previousTickArray[0],vi=parseInt(1000*Math.min(Math.abs(xw),Math.abs((1500.0/beaconSpeed)-xw)));
         el("E").textContent=vi;
         if(vi < 75 && userFreq==beaconFreq && userSpeed==beaconSpeed){
             el("F").style="";
-            el("S").style="";
         }
       },50);
-      el("A1").addEventListener("click",e=>{userFreq=userFreq>=600?600:userFreq+20;});
-      el("A2").addEventListener("click",e=>{userFreq=userFreq<=200?200:userFreq-20;});
-      el("B").addEventListener("click",e=>{userSpeed=userSpeed>=5?1:userSpeed+1;});
-      el("G").addEventListener("click",e=>{randomize();});
-      const recursiveBeacon=function recursiveBeacon(){
-        previousTickArray[0]=audioctx.currentTime;
-        playAudio(audioctx, beaconFreq);
-        window.setTimeout(()=>{recursiveBeacon();},1500.0/beaconSpeed);
-      }
-      const recursiveUser=function recursiveUser(){
-        previousTickArray[1]=audioctx.currentTime;
-        playAudio(audioctx, userFreq);
-        window.setTimeout(()=>{recursiveUser();},1500.0/userSpeed);
-      }
-      recursiveUser();
-      window.setTimeout(()=>{recursiveBeacon();},50*parseInt(Math.random()*55));
+el("A1").addEventListener("click",e=>{userFreq=userFreq>=600?600:userFreq+20;});
+el("A2").addEventListener("click",e=>{userFreq=userFreq<=200?200:userFreq-20;});
+el("B").addEventListener("click",e=>{userSpeed=userSpeed>=5?1:userSpeed+1;});
+el("G").addEventListener("click",e=>{randomize();});
+el("U1").addEventListener("click",e=>{
+    if(flux>=100*Math.pow(3,u1)){
+        flux-=100*Math.pow(3,u1)
+        u1++
+    }
+});
+const recursiveBeacon=function recursiveBeacon(){
+  previousTickArray[0]=audioctx.currentTime;
+  playAudio(audioctx, beaconFreq);
+  window.setTimeout(()=>{recursiveBeacon();},1500.0/beaconSpeed);
+}
+const recursiveUser=function recursiveUser(){
+  previousTickArray[1]=audioctx.currentTime;
+  playAudio(audioctx, userFreq);
+  window.setTimeout(()=>{recursiveUser();},1500.0/userSpeed);
+}
+recursiveUser();
+window.setTimeout(()=>{recursiveBeacon();},50*parseInt(Math.random()*55));
